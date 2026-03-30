1ebf64c3da5c0f59dfb4bb3a9c5ef59c3795850a
codewonder <46184224+code-wonder@users.noreply.github.com>
2026-03-30 01:32:28 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Fun 3D Volume Calculator")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start to find the volume of Geometry."
 )
